a6e4885e9c81f991e7c92b5d3c2c42612213e122
friend1019 <genie1019002@gmail.com>
2025-05-28 18:04:26 +0900

    axios랑 router 파일 추가 및 폴더들 생성

---
+++ src/Router.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Home from "./component/Home";
+
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
