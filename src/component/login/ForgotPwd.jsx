551ffecf204527ff8911b1914817fd7bdb47a238
yonheeee <nobrake0706@naver.com>
2025-09-25 18:23:29 +0900

    카카오 로그인 오류 문구 수정 완료

--- src/component/login/ForgotPwd.jsx
+++ src/component/login/ForgotPwd.jsx
@@ -45,7 +45,7 @@ const ForgotPwd = () => {
 
       if (code === "USER_NOT_FOUND") {
         setGeneralError("가입된 계정이 없습니다.");
-      } else if (code === "SOCIAL_LOGIN_USER" || code === "KAKAO_USER" || code === "SOCIAL_USER") {
+      } else if (code === "SOCIAL_LOGIN_USER" || code === "KAKAO_USER" || code === "SOCIAL_USER" || code === "KAKAO_ACCOUNT_LOGIN") {
         setGeneralError("해당 이메일은 카카오 로그인 전용 계정입니다. 카카오 로그인을 이용해주세요.");
       } else {
         setGeneralError("서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
